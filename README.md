029d3bbadb21b1e7ee298505a45187c025878995
Scott Lee <scott.lee@engie.com>
2020-05-26 11:58:09 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# VoiceBot - Mario 64
+
+Voicebot (https://www.voicebot.net/) is a tool that allows you to associate certain words with input commands on desktop PCs. The code in this repository, blj_voicebot.cs, is a C# macro for Voicebot written to perform rapid 'button mashing', or key presses, to simulate the jumping input needed for performing a backward long jump (BLJ).
+
+The contents of this repository fall under the MIT license, so please feel free to use this for your own purposes. 
+
+
+### Author
+My name is Scott Lee, and I wrote this code. You can learn more about the projects I've done like this and others at the Elega Corporation website: https://www.ElegaCorp.com! I'm an online educator and developer of games & business applications.
+
+Disclaimer: Elega Corporation does not own the rights to, or claim any intellectual property ownership of Voicebot. Voicebot is created by [Binrary Fortress Software](https://www.binaryfortress.com/).
---
+++ blj_voicebot.cs
@@ -0,0 +1,77 @@
+// Authored by Elega Corporation, 2020.
+// This code is under the MIT license: please see the LICENSE file regarding usage of this code.
+
+using System;
+using System.Drawing;
+
+public static class VoiceBotScript
+{
+    public static void wait(int timeToWait)
+    {
+        // To wait, just loop and count while 
+        // waiting to reach the timeToWait
+        // This hopefully translates to a delay
+        // in some number of milliseconds when
+        // executing...
+        for (int i = 0; i < timeToWait; i++)
+            continue;
+    }
+    
+    public static void Run(IntPtr windowHandle)
+    {
+        // This allows a flag to be set for wiring up to a stop command
+        BFS.ScriptSettings.WriteValueBool("Repeat_Jump", true);
+
+        // The number of times to mash the jump button
+        int numberOfKeyPresses = 40000;
+        int currentKeyPress = 0; // Initialize the starting key press at zero (or the first value)
+        int periodToWait = 25; // Number of cycles between key presses
+
+        // Then, break up the key press counts into 4 quartiles, so
+        // that when progressing through the key presses, it is possible
+        // to accelerate the button mashing to a higher speed.
+        int keyPressQuartileOne = numberOfKeyPresses / 4;
+        int keyPressQuartileTwo = numberOfKeyPresses / 2;
+        int keyPressQuartileThree = keyPressQuartileOne * 3;
+
+        // These true/false boolean flags allow us to know
+        // when we have hit the critical quartiles for 
+        // acceleration and then, once true, avoid repeatedly
+        // manipulating the periodToWait variable.
+        bool hitQuartileTwo = false;
+        bool hitQuartileThree = false;
+        
+        // While we have not run a stop command verbally, or while
+        // we have not reached the maximum number of button presses...
+        while(BFS.ScriptSettings.ReadValueBool("Repeat_Jump")
+            && currentKeyPress < numberOfKeyPresses)
+        {
+            // Mash the 'M' key (or the jump button)
+            BFS.Input.SendKeyDown("M");
+            BFS.Input.SendKeyUp("M");
+
+            // Count this as a key press against the maximum
+            currentKeyPress += 1;
+
+            // If we've hit the second quartile, increase our acceleration
+            // by reducing the delay in cycles between key presses
+            if (currentKeyPress <= keyPressQuartileTwo && !hitQuartileTwo)
+            {
+                periodToWait -= 4;
+                hitQuartileTwo = true;
+            }
+            else if (currentKeyPress <= keyPressQuartileThree && !hitQuartileThree)
+            {
+                // The same process is done again for the third quartile,
+                // but this time, only decrease the delay by 2 cycles...
+                periodToWait -= 2;
+                hitQuartileThree = true;
+            }
+
+            // Wait the period to wait, which could start at 28,
+            // or decrease based on the numbers in the conditionals
+            // written above
+            wait(periodToWait);
+        }
+    }
+}
